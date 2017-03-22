338f5afa101432a65d5113518c111b195f064fce
appukuttan-shailesh <appukuttan.shailesh@gmail.com>
2017-03-22 17:24:00 +0100

    Commit #1

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     version='0.0.2dev',
     author='Shailesh Appukuttan',
     author_email='shailesh.appukuttan@unic.cnrs-gif.fr',
-    packages=['tests'],
+    packages=['VF_test_cases', 'VF_test_cases.tests'],
     url='https://github.com/appukuttan-shailesh/VF_test_cases',
     license='MIT',
     description='A SciUnit library for data-driven validation of models having layers.',
